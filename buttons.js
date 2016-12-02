e1717aae68271f7234e44d4ebc5a5f0c49ca8922
Farhan Chandel <fchandel@sfu.ca>
2016-12-01 18:37:03 -0800

    fixed broken code

--- buttons.js
+++ buttons.js
@@ -1,4 +1,4 @@
 function buttons() {
-    document.getElementById(about).style.display="inline";
+    document.getElementById(about).style.display="block";
     //document.getElementById(buttonArea).style.display='block';
 }
