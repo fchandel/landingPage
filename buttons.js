9b2d612ec182ba701072ec80c2dcf1e859515aff
fchandel <farhanchandel@gmail.com>
2016-12-01 18:18:48 -0800

    Add files via upload

--- buttons.js
+++ buttons.js
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+function buttons() {
+    document.getElementById(about).style.display="inline";
+    //document.getElementById(buttonArea).style.display='block';
+}
+=======
 function buttons() {
         document.getElementById(button_area).style.display='inline';
 }
+>>>>>>> be165445fe1352eedd6f6e8130258a4e6f5e3d45
