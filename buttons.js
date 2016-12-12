e6f196a29d66fc81cbfc39c5dba5cb0c261276df
Farhan Chandel <fchandel@sfu.ca>
2016-12-12 14:31:39 -0800

    javascript working

--- buttons.js
+++ buttons.js
@@ -1,3 +1,11 @@
-function buttons() {
-  document.getElementById(name).style.display="block";
+function about() {
+  document.getElementById("hook").innerHTML = "<center><h1>Farhan Chandel</h1></br><h3>Software Systems Major</h3></center>";
+}
+
+function Intro() {
+  document.getElementById("hook").innerHTML = "<h1>Hello!</h1><h3>Its nice to finally meet you.</h3>";
+}
+
+function Contact() {
+  document.getElementById("hook").innerHTML = "<center><h1>Get in Touch.</h1></br>Email: Farhanchandel@gmail.com</br> Phone: 778-829-6205</br></center>";
 }
